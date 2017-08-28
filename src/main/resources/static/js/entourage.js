de4c523b515f2a3e1d76c7c67e4460b7e3794648
James Hartanto <jameshartanto1993@gmail.com>
2017-08-28 19:47:42 -0400

    bold selection on entourage bridesmaids or groomsmen

--- src/main/resources/static/js/entourage.js
+++ src/main/resources/static/js/entourage.js
@@ -8,15 +8,22 @@ var buttonGroom = document.getElementById("buttonGroom");
 var bridesmaids = document.getElementById("bridesmaids");
 var groomsmen = document.getElementById("groomsmen");
 
+var bridesmaidText = document.getElementById("bridesmaidText");
+var groomsmenText = document.getElementById("groomsmenText");
+
 buttonBride.addEventListener("click", showBride);
 buttonGroom.addEventListener("click", showGroom);
 
 function showBride() {
     bridesmaids.style.display = "block";
     groomsmen.style.display = "none";
+    bridesmaidText.style = "font-weight: bold";
+    groomsmenText.style = "font-weight: normal";
 }
 
 function showGroom() {
     bridesmaids.style.display = "none";
     groomsmen.style.display = "block";
+    bridesmaidText.style = "font-weight: normal";
+    groomsmenText.style = "font-weight: bold";
 }
