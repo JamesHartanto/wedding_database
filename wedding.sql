a2e5e4863a526bcf74a23fb7bd46a3c62c4fd392
James Hartanto <jameshartanto1993@gmail.com>
2017-08-31 13:24:04 -0400

    sent guestlist online

--- wedding.sql
+++ wedding.sql
@@ -89,8 +89,115 @@ ALTER TABLE ONLY guestinfo ALTER COLUMN id SET DEFAULT nextval('guestinfo_id_seq
 --
 
 COPY guestinfo (id, name, attending, food, guestname, guestfood, guestallow, allergies, guestallergies, roomblock, song, email) FROM stdin;
-8	test	\N	Fish	\N	Fish	\N	allergies	\N	\N	song	email@gmail.com
-7	nancy lee	Yes	Fish	-	Fish	t	allergies	guest allergies	Yes	songs	email@gmail.com
+2	Shady Lee	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+3	Shu Tong Lee	\N	\N	Sofy Lee	\N	t	\N	\N	\N	\N	\N
+4	Hady Hartanto	\N	\N	Wati Hartanto	\N	t	\N	\N	\N	\N	\N
+1	James Hartanto	Yes	Beef			\N					
+5	Thomas Darden	\N	\N	Jody Darden	\N	t	\N	\N	\N	\N	\N
+6	John Mazzarino	\N	\N	Linda Storm Mazzarino	\N	t	\N	\N	\N	\N	\N
+7	Edmund Waddill	\N	\N	Mollie Waddill	\N	t	\N	\N	\N	\N	\N
+8	JT Vaughn	\N	\N	Gracey Vaughn	\N	t	\N	\N	\N	\N	\N
+9	John Linger	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+10	Jim Fogleman	\N	\N	Mary-Edna Fogleman	\N	t	\N	\N	\N	\N	\N
+11	Jon Spear	\N	\N	Lauren Spear	\N	t	\N	\N	\N	\N	\N
+12	Brent Burgess	\N	\N	Barbara Burgess	\N	t	\N	\N	\N	\N	\N
+13	Tom McFadyen	\N	\N	Mrs McFadyen	\N	t	\N	\N	\N	\N	\N
+14	Vickie Harp	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+15	Elizabeth Merritt	\N	\N	John Merritt	\N	t	\N	\N	\N	\N	\N
+16	Wendy Carter	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+17	Amy Biggers	\N	\N	Shan Biggers	\N	t	\N	\N	\N	\N	\N
+18	Scott Steele	\N	\N	Jennifer Steele	\N	t	\N	\N	\N	\N	\N
+19	Bret Batchelder	\N	\N	Virginia Batchelder	\N	t	\N	\N	\N	\N	\N
+20	Andrea Meli	\N	\N	Rick Germain	\N	t	\N	\N	\N	\N	\N
+21	Trista Balmer	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+22	Oliver Pau	\N	\N	Penny Pau	\N	t	\N	\N	\N	\N	\N
+23	Dewey Weaver	\N	\N	Anne Weaver	\N	t	\N	\N	\N	\N	\N
+24	Jeany Kim	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+25	Rima Massou	\N	\N	Alex Barylskiy	\N	t	\N	\N	\N	\N	\N
+26	Chelsea Yin	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+27	Kevin Hong	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+28	CJ Cruz	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+29	Eileen Lee	\N	\N	Arthur Chang	\N	t	\N	\N	\N	\N	\N
+30	Jane Ha	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+31	David Wayne	\N	\N	Sandy Enriquez	\N	t	\N	\N	\N	\N	\N
+32	Jenna Nelson	\N	\N	James Aubrey	\N	t	\N	\N	\N	\N	\N
+33	Lisa Gilliland	\N	\N	Chris	\N	t	\N	\N	\N	\N	\N
+34	Lisa Chang	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+35	Lili Rodriguez	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+36	Rebeca Lopez	\N	\N	Benjamin Chung	\N	t	\N	\N	\N	\N	\N
+37	Gloria Mayorga	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+38	Naomi Choi	\N	\N	Jason Lee	\N	t	\N	\N	\N	\N	\N
+39	Andrea Wong	\N	\N	Matthew Lim	\N	t	\N	\N	\N	\N	\N
+40	Megan Boaglio	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+41	Alex LaRosa	\N	\N	Paul Williams	\N	t	\N	\N	\N	\N	\N
+42	Annika Skye	\N	\N	Josef John	\N	t	\N	\N	\N	\N	\N
+43	Leslie Koh	\N	\N	Joshua Koh	\N	t	\N	\N	\N	\N	\N
+44	Lauren Gunderson	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+45	Larissa Saco	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+46	Riley Hathaway	\N	\N	Season Hathaway	\N	t	\N	\N	\N	\N	\N
+47	David Huang	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+48	Megan Hoewisch	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+49	Ryan Hammill	\N	\N	Kathleen McKenzie	\N	t	\N	\N	\N	\N	\N
+50	Jonathan Kim	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+51	Sam Ovenshine	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+52	Esther Cajahuaringa	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+53	Nic Arnold	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+54	Lauren Masopust	\N	\N	Jeremiah Masopust	\N	t	\N	\N	\N	\N	\N
+55	Aiden Lewis	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+56	Adi Tanojo	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+57	Joshua Park	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+58	Christopher Lee	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+59	Eric Kim	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+60	Holly Goldingay	\N	\N	Jack	\N	t	\N	\N	\N	\N	\N
+61	Teresa Monti	\N	\N	Jack	\N	t	\N	\N	\N	\N	\N
+62	Emily Tran	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+63	Jessica Phan	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+64	Bonnie Groat	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+65	Nik Hasiuk	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+66	Jet Abdo	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+67	Geoff Villacis	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+68	Joe Kerrigan	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+69	James McCumiskey	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+70	Sam Beard	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+71	Heather Martin	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+72	Luke Currey	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+73	Kameel Andraos	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+74	Reyna Trimboli	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+75	Kevin Lau	\N	\N	Christine Ma Lau	\N	t	\N	\N	\N	\N	\N
+76	Nathan Chan	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+77	Hannah Chin	\N	\N	Joseph Lee	\N	t	\N	\N	\N	\N	\N
+78	Tim Richardson	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+79	Jon Pettigrew	\N	\N	Rachel Pettigrew	\N	t	\N	\N	\N	\N	\N
+80	Josh Pettigrew	\N	\N	Esther Pettigrew	\N	t	\N	\N	\N	\N	\N
+81	Joy Pettigrew	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+82	Boris Lau	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+83	Bjorn Li	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+84	Darren Wong	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+85	Hadrian Ho	\N	\N	Nikkie Sun	\N	t	\N	\N	\N	\N	\N
+86	Hulbert Soh	\N	\N	\N	\N	t	\N	\N	\N	\N	\N
+87	Joanne Soo	\N	\N	Lance Cotingkeh	\N	t	\N	\N	\N	\N	\N
+88	Emily Lai	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+89	Frances Su	\N	\N	Vincent Pai	\N	t	\N	\N	\N	\N	\N
+90	Hannah Yung	\N	\N	Erik Wiest	\N	t	\N	\N	\N	\N	\N
+91	Grace Chuang	\N	\N	Kenny	\N	t	\N	\N	\N	\N	\N
+92	Irene Wang	\N	\N	Connor	\N	t	\N	\N	\N	\N	\N
+93	Timothy Chang	\N	\N	Taylor Yang	\N	t	\N	\N	\N	\N	\N
+94	Gary Liou	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+95	Oliver Sheu	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+96	Daniel Cheng	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+97	Emily Yuan	\N	\N	Chris Lo	\N	t	\N	\N	\N	\N	\N
+98	Ernest Wang	\N	\N	May Ting	\N	t	\N	\N	\N	\N	\N
+99	Philip Cros	\N	\N	Hwang Eban	\N	t	\N	\N	\N	\N	\N
+100	Benjamin Farrell	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+101	Alyssa Montgomery	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+102	Rachel Garratt	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+103	John Lovato	\N	\N	Hayden Lovato	\N	t	\N	\N	\N	\N	\N
+104	Cody Mackvick	\N	\N	Cara	\N	t	\N	\N	\N	\N	\N
+105	Daniel Yelverton	\N	\N	Laney Yelverton	\N	t	\N	\N	\N	\N	\N
+106	Carlton Satterwhite	\N	\N	Brandy Satterwhite	\N	t	\N	\N	\N	\N	\N
+107	Kyle Gilley	\N	\N	Stephanie Gilley	\N	t	\N	\N	\N	\N	\N
+108	Ryan Shank	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+109	Matt Benson	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
 \.
 
 
@@ -98,7 +205,7 @@ COPY guestinfo (id, name, attending, food, guestname, guestfood, guestallow, all
 -- Name: guestinfo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: JamesHartanto
 --
 
-SELECT pg_catalog.setval('guestinfo_id_seq', 8, true);
+SELECT pg_catalog.setval('guestinfo_id_seq', 12, true);
 
 
 --
